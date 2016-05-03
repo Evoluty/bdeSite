77d437dc0e38f1d9619e4bdab4b92a96b00f1417
Obiwan1995 <brice.miclo@gmail.com>
2016-05-03 15:34:58 +0200

    Ajout des messages d'erreurs ou de validation dans les news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,24 +1,27 @@
 class NewsController < ApplicationController
   def index
   	@title = "News"
-	@page = 1
-	page = params[:page]
+  	@page = 1
+  	page = params[:page]
 	if !page.nil?
 		@page = page.to_i
 	end
-	nb_news_per_page = 2
+    nb_news_per_page = 2 
   	@news=News.paginate(:page => @page, :per_page => nb_news_per_page)
   end
 
   def create
   	if request.post?
-		title = params[:title]
-		text = params[:text]
-		a = News.new()
-		a.text = text
-		a.title = title
-		a.save()
-		redirect_to "/admin"
+  		title = params[:title]
+  		text = params[:text]
+  		a = News.new()
+  		a.text = text
+  		a.title = title
+  		if (a.save())
+  		  redirect_to "/admin", notice: "L'actualité a bien été ajoutée !"
+      else
+        redirect_to "/news/create", alert: "Tous les champs doivent être remplis !"
+      end
   	else
   		@title = "Ajouter une actualité"
   	end
@@ -27,6 +30,6 @@ class NewsController < ApplicationController
   def delete
   	id = params[:id]
   	News.destroy(id)
-  	redirect_to "/admin"
+  	redirect_to "/admin", notice: "L'actualité a bien été supprimée !"
   end
 end
