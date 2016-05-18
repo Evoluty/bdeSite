b5ce9a55b332b1fb93f03bde2a3068d478bf4d70
Obiwan1995 <brice.miclo@gmail.com>
2016-05-18 15:00:21 +0200

    Revert "Merge branch 'master' of https://github.com/Evoluty/bdeSite"
    This reverts commit 91f477d75872f2c68b758031c09a64f4ba4eedbf, reversing
    changes made to 349c98d4262dedac45897adbc3547b73c17a23de.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -6,8 +6,8 @@ class NewsController < ApplicationController
   	if !page.nil?
   		@page = page.to_i
   	end
-    nb_news_per_page = 5
-  	@news=News.paginate(:page => @page, :per_page => nb_news_per_page)
+    nb_news_per_page = 2 
+  	@news=News.paginate(:page => @page, :per_page => nb_news_per_page).order('created_at DESC')
   end
 
   def create
@@ -27,9 +27,23 @@ class NewsController < ApplicationController
     end
   end
 
+  def update
+    title = params[:title]
+    text = params[:text]
+    id = params[:id]
+    a = News.find(id)
+    a.title = title
+    a.text = text
+    if (a.save())
+      render :text => 1
+    else
+      render :text => 0
+    end
+  end
+
   def delete
   	id = params[:id]
   	News.destroy(id)
-  	redirect_to "/admin", notice: "L'actualité a bien été supprimée !"
+    render :nothing => true
   end
 end
